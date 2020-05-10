52dc66a44bbb378a603a36c7da3555b7be28e762
Russell Hallk <owner@russellhall.co.uk>
2020-05-11 00:07:57 +0100

    k3os cluster

--- terraform/node4/data.tf
+++ terraform/node4/data.tf
@@ -12,36 +12,6 @@ data "vsphere_network" "network" {
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-data "vsphere_virtual_machine" "template" {
-  name          = "${var.template_name}"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-data "vsphere_virtual_machine" "k3os" {
-  name          = "packer-vmware-iso"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-data "vsphere_virtual_machine" "k3os-a" {
-  name          = "k3os-a-template"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-data "vsphere_virtual_machine" "k3os-b" {
-  name          = "k3os-b-template"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-data "vsphere_virtual_machine" "k3os-c" {
-  name          = "k3os-c-template"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-data "vsphere_virtual_machine" "k3os-d" {
-  name          = "k3os-d-template"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
 data "vsphere_compute_cluster" "cluster" {
   name          = "LAB"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
--- terraform/node4/main.tf
+++ terraform/node4/main.tf
@@ -11,60 +11,102 @@ resource "vsphere_virtual_machine" "k3os-a" {
   name             = "${var.virtual_machine_name_prefix}-a"
   resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
-
-  num_cpus = 4
-  memory   = 8192
-  guest_id = "${data.vsphere_virtual_machine.k3os-a.guest_id}"
-
+  num_cpus         = 4
+  memory           = 8192
+  guest_id         = "other3xLinux64Guest"
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
   }
-
   disk {
     label = "disk0"
     size  = 64
   }
+  # disk {
+  #   label = "disk1"
+  #   size  = 300
+  #   thin_provisioned = true
+  #   unit_number = 1
+  # }
+  cdrom {
+    datastore_id = "${data.vsphere_datastore.datastore.id}"
+    path         = "ISOs/k3os-a.iso"
+  }
+}
 
+resource "vsphere_virtual_machine" "k3os-b" {
+  name             = "${var.virtual_machine_name_prefix}-b"
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  num_cpus         = 4
+  memory           = 8192
+  guest_id         = "other3xLinux64Guest"
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+  disk {
+    label = "disk0"
+    size  = 64
+  }
   disk {
     label = "disk1"
     size  = 300
     thin_provisioned = true
     unit_number = 1
   }
-
   cdrom {
-    client_device = true
-  }
-
-  clone {
-    template_uuid = "${data.vsphere_virtual_machine.k3os-a.id}"
+    datastore_id = "${data.vsphere_datastore.datastore.id}"
+    path         = "ISOs/k3os-b.iso"
   }
 }
 
-# resource "vsphere_virtual_machine" "k3os" {
-#   count            = "1"
-#   name             = "${var.virtual_machine_name_prefix}${count.index+4}"
-#   resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
-#   datastore_id     = "${data.vsphere_datastore.datastore.id}"
-#
-#   num_cpus = 4
-#   memory   = 8192
-#   guest_id = "${data.vsphere_virtual_machine.k3os.guest_id}"
-#
-#   network_interface {
-#     network_id = "${data.vsphere_network.network.id}"
-#   }
-#
-#   disk {
-#     label = "disk0"
-#     size  = 30
-#   }
-#
-#   cdrom {
-#     client_device = true
-#   }
-#
-#   clone {
-#     template_uuid = "${data.vsphere_virtual_machine.k3os.id}"
-#   }
-# }
+resource "vsphere_virtual_machine" "k3os-c" {
+  name             = "${var.virtual_machine_name_prefix}-c"
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  num_cpus         = 4
+  memory           = 8192
+  guest_id         = "other3xLinux64Guest"
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+  disk {
+    label = "disk0"
+    size  = 64
+  }
+  disk {
+    label = "disk1"
+    size  = 300
+    thin_provisioned = true
+    unit_number = 1
+  }
+  cdrom {
+    datastore_id = "${data.vsphere_datastore.datastore.id}"
+    path         = "ISOs/k3os-c.iso"
+  }
+}
+
+resource "vsphere_virtual_machine" "k3os-d" {
+  name             = "${var.virtual_machine_name_prefix}-d"
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  num_cpus         = 4
+  memory           = 8192
+  guest_id         = "other3xLinux64Guest"
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+  disk {
+    label = "disk0"
+    size  = 64
+  }
+  disk {
+    label = "disk1"
+    size  = 300
+    thin_provisioned = true
+    unit_number = 1
+  }
+  cdrom {
+    datastore_id = "${data.vsphere_datastore.datastore.id}"
+    path         = "ISOs/k3os-d.iso"
+  }
+}
